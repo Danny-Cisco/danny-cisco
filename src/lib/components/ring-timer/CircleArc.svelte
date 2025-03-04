3a002d4bfa72b31e1d2c3eca4e926977e4f97097
Danny Cisco <gumballelectronics@gmail.com>
2025-03-04 23:26:30 +1100

    created a timer disc

---
+++ src/lib/components/ring-timer/CircleArc.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let radius = 110;
+	export let arc = 0.9;
+	export let color = 'white';
+	export let stroke = 20;
+	export let width = 300;
+	export let height = 300;
+
+	let circum = 2 * radius * 3.14;
+</script>
+
+<circle
+	cx={width / 2}
+	cy={height / 2}
+	r={radius}
+	fill="none"
+	stroke={color}
+	stroke-width={stroke}
+	stroke-linecap="round"
+	stroke-dasharray="{arc * circum} {circum}"
+	transform="rotate(-90, {width / 2}, {height / 2})"
+/>
--- src/routes/timeblindclub/+page.svelte
+++ src/routes/timeblindclub/+page.svelte
@@ -1,17 +1,30 @@
-<div class="h-full w-full bg-gray-800">
+<script>
+	import CircleArc from '$lib/components/ring-timer/CircleArc.svelte';
+
+	let padding = 10;
+	let stroke = 20;
+	let gap = 5;
+	let innerRadius = 50;
+	let height = 12 * stroke + 10 * gap + 2 * innerRadius + padding;
+	console.log('🚀 ~ height:', height);
+	let width = height;
+
+	let ringsRadius = [];
+
+	let timers = [1, 1, 1, 1, 1, 0.5];
+
+	for (let i = 0; i < 6; i++) {
+		ringsRadius[i] = innerRadius + i * stroke + i * gap;
+	}
+</script>
+
+<div class="h-full w-full bg-black">
 	<div class="flex h-full flex-col items-center justify-center">
-		<svg height="300" width="300">
-			<circle
-				cx="150"
-				cy="150"
-				r="140"
-				fill="none"
-				stroke="white"
-				stroke-width="20"
-				stroke-linecap="round"
-				stroke-dasharray="{0.4 * 3.14 * 140 * 2} {3.14 * 140 * 2}"
-				transform="rotate(-90, 150, 150)"
-			/>
+		<svg {height} {width} class="">
+			{#each ringsRadius as radius, index}
+				<CircleArc {height} {width} color="#555555" arc="1" {radius} />
+				<CircleArc {height} {width} {radius} arc={timers[index]} />
+			{/each}
 		</svg>
 	</div>
 </div>
