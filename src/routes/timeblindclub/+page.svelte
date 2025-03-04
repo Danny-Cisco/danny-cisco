3dbd08aacefa25fb40f87d04d606f014bc850815
Danny Cisco <gumballelectronics@gmail.com>
2025-03-05 00:34:48 +1100

    multiple circles working

--- src/routes/timeblindclub/+page.svelte
+++ src/routes/timeblindclub/+page.svelte
@@ -1,13 +1,21 @@
 <script>
 	import FullCircle from '$lib/components/ring-timer/FullCircle.svelte';
-	let nowMins = 27;
+	let nowMins = 90;
+
+	// Function to generate an array of hours and remaining minutes
+	$: hoursArray = Array(Math.floor(nowMins / 60)).fill(60);
+	$: console.log('🚀 ~ hoursArray:', hoursArray);
+	$: remainder = nowMins % 60;
+	$: if (remainder > 0) hoursArray.push(remainder);
 </script>
 
 <div class="h-full w-full bg-black">
 	<input type="number" bind:value={nowMins} />
-	<div class="flex h-full flex-col items-center justify-center">
-		{#key nowMins}
-			<FullCircle {nowMins} />
-		{/key}
-	</div>
+	{#key nowMins}
+		<div class="flex flex-wrap items-center justify-center">
+			{#each hoursArray as mins, index}
+				<FullCircle nowMins={mins} />
+			{/each}
+		</div>
+	{/key}
 </div>
