fc0e40a22dd5ac7600874ab11dd722450b8776c1
Danny Cisco <gumballelectronics@gmail.com>
2025-03-05 12:11:16 +1100

    timer counts down every second

--- src/routes/timeblindclub/+page.svelte
+++ src/routes/timeblindclub/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import FullCircle from '$lib/components/ring-timer/FullCircle.svelte';
 
+	import { onMount } from 'svelte';
+	let interval;
+	let seconds = 0;
+
 	let timerDuration = 67;
 	let nowMins = 90;
 
@@ -22,6 +26,18 @@
 			hoursArray[i] = Math.round(timerDuration % 60) || 60; // make the last disc the remainder or 60 if no remainder
 		}
 	}
+
+	$: if (nowMins) nowMins = timerDuration - seconds;
+	$: console.log('🚀 ~ seconds:', seconds);
+	$: console.log('🚀 ~ nowMins:', nowMins);
+
+	onMount(() => {
+		interval = setInterval(() => {
+			seconds += 1;
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
 </script>
 
 <div class="flex h-full w-full flex-col items-center bg-black pt-10">
