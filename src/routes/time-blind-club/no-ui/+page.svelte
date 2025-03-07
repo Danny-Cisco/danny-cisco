14933672ff683e499f17c8c989ceaae16945da4f
Danny Cisco <gumballelectronics@gmail.com>
2025-03-07 14:43:13 +1100

    added my own btn class

---
+++ src/routes/time-blind-club/no-ui/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let startDateObj = null;
+	let nowDateObj = null;
+	let endDateObj = null;
+
+	let startMs;
+	$: startMs = startDateObj?.getTime();
+	$: console.log('🚀 ~ startMs:', startMs);
+
+	let nowMs;
+	$: nowMs = nowDateObj?.getTime();
+	$: console.log('🚀 ~ nowMs:', nowMs);
+
+	let endMs;
+	$: endMs = endDateObj?.getTime();
+	$: console.log('🚀 ~ endMs:', endMs);
+
+	onMount(async () => {
+		nowDateObj = new Date();
+		startDateObj = new Date();
+		endDateObj = new Date();
+	});
+</script>
+
+<div class="mt-4 flex w-full flex-col items-center">
+	<h1 class="text-center">No Ui</h1>
+	<button class="btn" on:click={() => (nowDateObj = new Date())}>getNow</button>
+</div>
