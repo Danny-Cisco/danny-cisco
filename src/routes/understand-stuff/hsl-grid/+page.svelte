260fbccff79695c2af173b5605b8783ab83d0e07
Danny Cisco <gumballelectronics@gmail.com>
2025-03-08 14:03:54 +1100

    improved hsl grid

--- src/routes/understand-stuff/hsl-grid/+page.svelte
+++ src/routes/understand-stuff/hsl-grid/+page.svelte
@@ -7,11 +7,19 @@
 
 	let selectedColor = null;
 	let showModal = false;
+	let copySuccess = false;
 
 	const selectColor = (h, l, s) => {
 		selectedColor = `hsl(${Math.round(h)}, ${Math.round(s)}%, ${Math.round(l)}%)`;
-		navigator.clipboard.writeText(selectedColor);
 		showModal = true;
+	};
+
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(selectedColor);
+		copySuccess = true;
+		setTimeout(() => {
+			copySuccess = false;
+		}, 2000);
 	};
 
 	const closeModal = (event) => {
@@ -22,6 +30,10 @@
 
 	onMount(() => {
 		window.addEventListener('keydown', closeModal);
+
+		return () => {
+			window.removeEventListener('keydown', closeModal);
+		};
 	});
 </script>
 
@@ -65,12 +77,62 @@
 </div>
 
 {#if showModal}
-	<div class="modal" on:click={() => (showModal = false)}>
-		<p>{selectedColor}</p>
+	<div class="modal" on:click|self={() => (showModal = false)}>
+		<button
+			class="absolute -right-5 -top-10 mt-4 flex h-10 w-10 flex-col items-center justify-center rounded-full bg-red-500 text-white"
+			on:click={() => (showModal = false)}
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+			</svg>
+		</button>
+
 		<p
-			style="background-color: {selectedColor}; width: 100%; height: 50px; border: 1px solid black;"
+			style="background-color: {selectedColor}; width: 300px; height: 500px; border: 1px solid black;"
 		></p>
-		<button class="btn mt-4" on:click={() => (showModal = false)}>Close</button>
+		<div class="h-10"></div>
+		<div class="flex items-center justify-center gap-2 text-white">
+			<p>{selectedColor}</p>
+			<button
+				on:click={copyToClipboard}
+				class="flex h-8 w-8 items-center justify-center rounded-full hover:bg-gray-700"
+				title="Copy to clipboard"
+			>
+				{#if copySuccess}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-5 text-green-500"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+					</svg>
+				{:else}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-5"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+						/>
+					</svg>
+				{/if}
+			</button>
+		</div>
 	</div>
 {/if}
 
@@ -94,7 +156,7 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-		background: white;
+		background: black;
 		padding: 20px;
 		border-radius: 8px;
 		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
