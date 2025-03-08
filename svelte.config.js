e0a5c338d7799acc31919632eafc9f87f0787c48
Danny Cisco <gumballelectronics@gmail.com>
2025-03-08 20:07:03 +1100

    disable warnings in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,24 @@ const config = {
 	},
 
 	// preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx'],
+
+	// Add this onwarn handler to ignore specific warnings
+	compilerOptions: {
+		onwarn: (warning, handler) => {
+			// Ignore specific warnings
+			if (
+				warning.code === 'css-unused-selector' ||
+				warning.code === 'a11y-click-events-have-key-events' ||
+				warning.code === 'a11y-no-noninteractive-element-interactions'
+			) {
+				return;
+			}
+
+			// Call the default handler for all other warnings
+			handler(warning);
+		}
+	}
 };
 
 export default config;
