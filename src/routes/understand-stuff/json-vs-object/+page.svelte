e7ffcdc188752589628e78412a2ca59daca9c11b
Danny Cisco <gumballelectronics@gmail.com>
2025-03-16 16:07:43 +1100

    finished my json cheat sheet

--- src/routes/understand-stuff/json-vs-object/+page.svelte
+++ src/routes/understand-stuff/json-vs-object/+page.svelte
@@ -53,6 +53,78 @@
 			<p>Cannot include functions</p>
 		</div>
 	</div>
+	<hr class="mt-10 w-full border-t-white" />
+	<div class="mx-auto mt-10 flex w-full max-w-lg gap-2">
+		<div class="js-key">Object</div>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+			/>
+		</svg>
+
+		<div class="">JSON.stringify(<span class="js-key">Object</span>)</div>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+			/>
+		</svg>
+
+		<div class="json-key">JSON string</div>
+	</div>
+	<div class="mx-auto mt-4 flex w-full max-w-lg gap-2">
+		<div class="json-key">JSON string</div>
+
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+			/>
+		</svg>
+
+		<div class="">JSON.parse(<span class="json-key">JSON string</span>)</div>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+			/>
+		</svg>
+
+		<div class="js-key">Object</div>
+	</div>
 </div>
 
 <style>
